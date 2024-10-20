4501cead9642b356aa34aae1a100aa5154043ad8
xgorrokx <durundukxx@gmail.com>
2024-10-20 19:01:37 +0300

    add files

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const devServer = (isDev) => !isDev ? {} : {
+    devServer: {
+        open: true,
+        hot: true,
+        port: 8080,
+    }
+}
+
+module.exports = ({ develop }) => ({
+    mode: develop ? 'development' : 'production',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: './styles/main.css'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(png|svg|jpg|jpeg)$/i,
+                type: 'asset/inline',
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader, 'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ]
+    },
+    ...devServer(develop),
+});
